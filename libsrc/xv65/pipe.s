ca1dd2d0c3be2c893ab8996e9a7d4ede6c59c9bf
Emanuele Fornara <efornara@users.sourceforge.net>
2013-10-02 20:27:33 +0100

    xv65: added missing pipe.s

---
+++ libsrc/xv65/pipe.s
@@ -0,0 +1,35 @@
+; pipe.s
+; Written by Emanuele Fornara
+
+	.export		_pipe
+	.import		xv65_ret_res_error
+	.import		xv65_ret_res_ok
+	.importzp	ptr1
+
+	.include	"xv65.inc"
+
+.segment	"CODE"
+
+; int __fastcall__ pipe(int *p)
+_pipe:
+	sta	ptr1
+	stx	ptr1+1
+	ldy	#REQ_PIPE
+	sty	REQPUT
+	sta	REQEND
+	lda	REQRES
+	beq	@r0
+	jmp	xv65_ret_res_error
+@r0:	ldy	#0
+	lda	REQDAT
+	sta	(ptr1),y
+	iny
+	lda	#0
+	sta	(ptr1),y
+	iny
+	lda	REQDAT+8
+	sta	(ptr1),y
+	iny
+	lda	#0
+	sta	(ptr1),y
+	jmp	xv65_ret_res_ok
