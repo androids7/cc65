130e2ee17f4d7ca0f0ad2b47e52cc95a283deadb
Emanuele Fornara <efornara@users.sourceforge.net>
2014-08-17 09:43:31 +0100

    primo: startup code optimized for rom

--- libsrc/primo/crt0.s
+++ libsrc/primo/crt0.s
@@ -1,33 +1,32 @@
 ;
 ; Written by Emanuele Fornara
 ;
-; C runtime startup code
+; C runtime startup code (RAM)
 ;
 
-	.export		_exit
-        .export         __STARTUP__ : absolute = 1      ; Mark as startup
+	.export __STARTUP__ : absolute = 1 ; Mark as startup
 
-	.import		__RAM_START__, __RAM_SIZE__
+	.import __CONSTRUCTOR_TABLE__, __CONSTRUCTOR_COUNT__
+	.import __DESTRUCTOR_TABLE__, __DESTRUCTOR_COUNT__
 
-	.importzp	sp
-	.import		initlib, donelib, callmain
+	.import start1
+	.import _main
 
-	.include	"jbit.inc"
+	.include "jbit.inc"
+	.include "header.s"
 
+	.segment "STARTUP"
 
-.include	"header.s"
+start0:
+	lda #<prog_info
+	ldx #>prog_info
+	jmp start1
 
+	.segment "RODATA"
 
-.segment       	"STARTUP"
-
-start:
-	lda	#<(__RAM_START__ + __RAM_SIZE__)
-	sta	sp
-	lda	#>(__RAM_START__ + __RAM_SIZE__)
-	sta	sp+1
-	jsr	initlib
-       	jsr    	callmain
-_exit:
-	jsr	donelib
-   	brk
-
+prog_info:
+	.word _main
+	.word __CONSTRUCTOR_TABLE__
+	.byte <__CONSTRUCTOR_COUNT__
+	.word __DESTRUCTOR_TABLE__
+	.byte <__DESTRUCTOR_COUNT__
