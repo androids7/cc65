cc86c7120a6893bffe166a3c96001d765cd7c3e5
Emanuele Fornara <efornara@users.sourceforge.net>
2017-06-06 20:37:01 +0200

    io2: added internal gotoxy

--- libsrc/io2/gotoxy.s
+++ libsrc/io2/gotoxy.s
@@ -5,15 +5,17 @@
 ; void gotoxy (unsigned char x, unsigned char y);
 ;
 
-	.export		_gotoxy
+	.export		gotoxy, _gotoxy
 	.import		popa
 
-        .include        "io2.inc"
+	.include	"io2.inc"
 
+gotoxy:
+	jsr popa	; Get Y
 _gotoxy:
-	sta	CONCY		; Set Y
-	jsr 	popa		; Get X
-	sta	CONCX		; Set X
+	sta CONCY	; Set Y
+	jsr popa	; Get X
+	sta CONCX	; Set X
 	rts
 
 
