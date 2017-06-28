d406ed04f7640837cbd186b1751353f7ea04e1be
Emanuele Fornara <efornara@users.sourceforge.net>
2017-06-28 19:35:54 +0200

    io2: added background color to iload.s sample

--- samples/io2/io1/iload.s
+++ samples/io2/io1/iload.s
@@ -8,6 +8,8 @@
 	sta	REQPTRHI
 	lda	#ENABLE_BGCOL|ENABLE_BGIMG
 	sta	ENABLE
+	lda	#<setbgcol
+	sta	REQPTRLO
 	lda	#<iload
 	sta	REQPTRLO
 	lda	#<setbgimg
@@ -18,6 +20,11 @@
 _data_start_:
 
 IMAGE_ID = 1
+
+setbgcol: req
+	.byte REQ_SETBGCOL
+	.byte 50, 150, 50
+endreq
 
 iload: req
 	.byte	REQ_ILOAD, IMAGE_ID, "/icon.png", 0
